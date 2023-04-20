108037c02c5181e0f9d1a6770a53387a619637a0
Ludo <ludovico.ferrara@ruhr-uni-bochum.de>
2023-04-20 19:58:00 +0200

    added base layout for landing screen with placeholder buttons

--- app/src/main/java/com/example/a07/MainActivity.java
+++ app/src/main/java/com/example/a07/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.a07;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button_questionnaire = (Button) findViewById(R.id.button_questionnaire);
+        button_questionnaire.setOnClickListener(v -> Log.d("BUTTONS", "User tried to open the questionnaire"));
+
+        Button button_tracking = (Button) findViewById(R.id.button_tracking);
+        button_tracking.setOnClickListener(v -> Log.d("BUTTONS", "User tried to open the tracking"));
+
+        Button button_archive = (Button) findViewById(R.id.button_archive);
+        button_archive.setOnClickListener(v -> Log.d("BUTTONS", "User tried to open the archive"));
+
+        Button button_settings = (Button) findViewById(R.id.button_settings);
+        button_settings.setOnClickListener(v -> Log.d("BUTTONS", "User tried to open the settings"));
     }
+
 }
