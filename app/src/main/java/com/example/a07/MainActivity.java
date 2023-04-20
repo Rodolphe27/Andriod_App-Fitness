18d2520ac5e9e1e4d6f24b0c625b31b9e7d68ec4
Ludo <ludovico.ferrara@ruhr-uni-bochum.de>
2023-04-20 23:14:13 +0200

    added `BottomNavigationView`

--- app/src/main/java/com/example/a07/MainActivity.java
+++ app/src/main/java/com/example/a07/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.a07;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Find buttons by ID and set an OnClickListener to log a message when the button is clicked
         Button button_questionnaire = (Button) findViewById(R.id.button_questionnaire);
         button_questionnaire.setOnClickListener(v -> Log.d("BUTTONS", "User tried to open the questionnaire"));
 
@@ -24,6 +26,18 @@ public class MainActivity extends AppCompatActivity {
 
         Button button_settings = (Button) findViewById(R.id.button_settings);
         button_settings.setOnClickListener(v -> Log.d("BUTTONS", "User tried to open the settings"));
-    }
 
+        // Do the same for navBar buttons
+        ImageButton buttonQuestionnaire = (ImageButton) findViewById(R.id.buttonQuestionnaire);
+        buttonQuestionnaire.setOnClickListener(v -> Log.d("BUTTONS", "User tried to open the questionnaire through the navBar"));
+
+        ImageButton ButtonTracking = (ImageButton) findViewById(R.id.buttonTracking);
+        ButtonTracking.setOnClickListener(v -> Log.d("BUTTONS", "User tried to open the tracking through the navBar"));
+
+        ImageButton buttonArchive = (ImageButton) findViewById(R.id.buttonArchive);
+        buttonArchive.setOnClickListener(v -> Log.d("BUTTONS", "User tried to open the archive through the navBar"));
+
+        ImageButton buttonSettings = (ImageButton) findViewById(R.id.buttonSettings);
+        buttonSettings.setOnClickListener(v -> Log.d("BUTTONS", "User tried to open the settings through the navBar"));
+    }
 }
