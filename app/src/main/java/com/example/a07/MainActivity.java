9ec0a7c3b2e1551608939dd8813d11c60ba9d388
Minh <94862685+luiminyan@users.noreply.github.com>
2023-04-22 00:03:42 +0200

    created 4 dummy pages fro the functions, and connections between pages are tested

--- app/src/main/java/com/example/a07/MainActivity.java
+++ app/src/main/java/com/example/a07/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.a07;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,7 +17,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Find buttons by ID and set an OnClickListener to log a message when the button is clicked
+        //By Minhua L: realized the "log a message function" by using toasts(below)
+/*        // Find buttons by ID and set an OnClickListener to log a message when the button is clicked
         Button button_questionnaire = (Button) findViewById(R.id.button_questionnaire);
         button_questionnaire.setOnClickListener(v -> Log.d("BUTTONS", "User tried to open the questionnaire"));
 
@@ -26,8 +30,7 @@ public class MainActivity extends AppCompatActivity {
 
         Button button_settings = (Button) findViewById(R.id.button_settings);
         button_settings.setOnClickListener(v -> Log.d("BUTTONS", "User tried to open the settings"));
-
-        // Do the same for navBar buttons
+        //  Do the same for navBar buttons
         ImageButton buttonQuestionnaire = (ImageButton) findViewById(R.id.buttonQuestionnaire);
         buttonQuestionnaire.setOnClickListener(v -> Log.d("BUTTONS", "User tried to open the questionnaire through the navBar"));
 
@@ -38,6 +41,34 @@ public class MainActivity extends AppCompatActivity {
         buttonArchive.setOnClickListener(v -> Log.d("BUTTONS", "User tried to open the archive through the navBar"));
 
         ImageButton buttonSettings = (ImageButton) findViewById(R.id.buttonSettings);
-        buttonSettings.setOnClickListener(v -> Log.d("BUTTONS", "User tried to open the settings through the navBar"));
+        buttonSettings.setOnClickListener(v -> Log.d("BUTTONS", "User tried to open the settings through the navBar"));*/
+    }
+
+
+    public void openQuestionnaire(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_archive_main, Toast.LENGTH_SHORT);    //toast a text when open
+        toast.show();
+        Intent intent = new Intent(this, Questionnaire.class);      //intent created to open a new page (activity)
+        startActivity(intent);
+    }
+
+    public void openSettings(View view){
+        Toast toast = Toast.makeText(this, R.string.toast_settings_main, Toast.LENGTH_SHORT);    //toast a text when open
+        toast.show();
+        Intent intent = new Intent(this, Settings.class);
+        startActivity(intent);
+    }
+
+    public void openTracking(View view){
+        Toast toast = Toast.makeText(this, R.string.toast_tracking_main, Toast.LENGTH_SHORT);    //toast a text when open
+        toast.show();
+        Intent intent = new Intent(this, SportTracking.class);
+        startActivity(intent);
+    }
+    public void openArchive(View view){
+        Toast toast = Toast.makeText(this, R.string.toast_archive_main, Toast.LENGTH_SHORT);    //toast a text when open
+        toast.show();
+        Intent intent = new Intent(this, Archive.class);
+        startActivity(intent);
     }
 }
