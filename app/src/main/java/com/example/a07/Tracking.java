fcad50290917d79f7fee2d7bb45990c63ab5fdcd
Minh <94862685+luiminyan@users.noreply.github.com>
2023-05-15 15:16:37 +0200

    sport activity main UI done by minhua

--- app/src/main/java/com/example/a07/Tracking.java
+++ app/src/main/java/com/example/a07/Tracking.java
@@ -3,15 +3,28 @@ package com.example.a07;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Tracking extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sport_tracking);
+
+        //set listener for btn changing to sport tracking page 1
+        findViewById(R.id.btnSportPage).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(Tracking.this, sportTest02.class);
+//                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
     }
 
     // Methods to switch activity
