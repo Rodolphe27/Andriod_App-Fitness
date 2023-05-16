44616ffb8e796fca53d91c1fa5b2a0758b49be13
Shipan <liushipan1998@gmail.com>
2023-05-16 18:02:57 +0200

    database for soprt set

--- app/src/main/java/com/example/a07/Tracking.java
+++ app/src/main/java/com/example/a07/Tracking.java
@@ -1,21 +1,23 @@
 package com.example.a07;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Chronometer;
+import android.widget.SeekBar;
 import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.a07.dao.SportDao;
-import com.example.a07.entity.QuestionaireEntity;
 import com.example.a07.entity.SportEntity;
 import com.example.a07.utils.SharedPreferencesUtil;
 import com.example.a07.utils.Utils;
@@ -23,7 +25,7 @@ import com.example.a07.utils.Utils;
 import java.util.Calendar;
 import java.util.List;
 
-public class Tracking extends AppCompatActivity implements View.OnClickListener {
+public class Tracking extends AppCompatActivity implements View.OnClickListener, SeekBar.OnSeekBarChangeListener {
 
     private SportDao sportDao;
 
@@ -32,10 +34,18 @@ public class Tracking extends AppCompatActivity implements View.OnClickListener 
     private Spinner sportTypeSpinner;
     private TextView currentDate;
     private Calendar calendar;
-    private long stopTime;
-    protected long stopTimeStored;
+    private long stopTime = 0;
+
+    private long startTime = 0;
+    private long pauseTime = 0;
+
+    private long timeBuf = 0;
 
     private Button startBtn, stopBtn, pauseBtn, addSportBtn;
+
+    private String sportName;
+    private int myMoodScore; // moodscore after sport
+
 
 
     @Override
@@ -53,8 +63,7 @@ public class Tracking extends AppCompatActivity implements View.OnClickListener 
         sportTypeSpinner = findViewById(R.id.spinnerSportType);
         currentDate = findViewById(R.id.tvShowCurrentDate);
         chronometer = findViewById(R.id.chronometerSport);
-        stopTime = 0;
-        stopTimeStored = 0;
+
 
         //set current date
         calendar = Calendar.getInstance();
@@ -74,6 +83,7 @@ public class Tracking extends AppCompatActivity implements View.OnClickListener 
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 String content = parent.getItemAtPosition(position).toString();
+                sportName = content;
                 Utils.showToast(Tracking.this, "The selected sport is: " + content);
             }
 
@@ -88,6 +98,8 @@ public class Tracking extends AppCompatActivity implements View.OnClickListener 
             public void onClick(View view) {
                 chronometer.setBase(SystemClock.elapsedRealtime() + stopTime);
                 chronometer.start();
+
+                startTime = System.currentTimeMillis();
             }
         });
 
@@ -96,6 +108,11 @@ public class Tracking extends AppCompatActivity implements View.OnClickListener 
             public void onClick(View view) {
                 chronometer.stop();
                 stopTime =  chronometer.getBase() - SystemClock.elapsedRealtime();
+
+
+                pauseTime = System.currentTimeMillis();
+                timeBuf += pauseTime - startTime;
+
             }
         });
 
@@ -103,9 +120,12 @@ public class Tracking extends AppCompatActivity implements View.OnClickListener 
             @Override
             public void onClick(View view) {
                 chronometer.stop();
-                stopTimeStored = stopTime;      //store stopTime in stopTimeStored (for later use)
-                stopTime = 0; // reset stop time
+                stopTime = 0;
+                timeBuf = System.currentTimeMillis() - pauseTime;
+                timeBuf = timeBuf/1000/60;
+                Utils.showToast(Tracking.this, Long.toString(timeBuf));
                 chronometer.setBase(SystemClock.elapsedRealtime());
+                showDialogSeekBar();
             }
         });
 
@@ -146,6 +166,69 @@ public class Tracking extends AppCompatActivity implements View.OnClickListener 
     }
 
 
+    private void showDialogSeekBar() {
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder(this);
+        View mView = getLayoutInflater().inflate(R.layout.dialog_seekbar, null);
+        // mySeekBar
+        SeekBar mySeekBar = mView.findViewById(R.id.myMoodScoreSeekBar);
+        mySeekBar.setOnSeekBarChangeListener(this);
+        mBuilder.setTitle(R.string.after_sport_seekbar);
+        //put the seekbar into the dialog
+        mBuilder.setView(mView);
+
+        mBuilder.setPositiveButton(R.string.btn_after_sport_dialog_positive, (dialog, which) -> {
+            // save to datebase;
+            saveSportDataToDB();
+            // todo? start questionaire?
+
+        });
+
+        mBuilder.setNegativeButton(R.string.btn_after_sport_dialog_negative, (dialog, which) -> {
+           // back ot main page
+            Intent intent = new Intent(Tracking.this, MainActivity.class);
+            startActivity(intent);
+        });
+
+
+        //show the AlertDialog using show() method
+        AlertDialog alertDialog2 = mBuilder.create();
+        alertDialog2.show();
+    }
+
+
+    /*here is for mySeekBar.setOnSeekBarChangeListener(this)*/
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+        myMoodScore = seekBar.getProgress();
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+
+    private void saveSportDataToDB() {
+        // create SportEntity
+        SportEntity sportEntity = new SportEntity();
+        sportEntity.setTimeAndDateStamp(Utils.getCurrentDateAndTime());
+        sportEntity.setName(sportName);
+        sportEntity.setDuration(Long.toString(timeBuf));
+        sportEntity.setMoodScore(myMoodScore);
+
+        sportDao.insert(sportEntity);
+        Utils.showToast(Tracking.this, "Sport Saved");
+    }
+
+
+
+
 
     // Methods to switch activity
     public void switchActivity(View view) {
@@ -163,4 +246,6 @@ public class Tracking extends AppCompatActivity implements View.OnClickListener 
         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intent);
     }
+
+
 }
