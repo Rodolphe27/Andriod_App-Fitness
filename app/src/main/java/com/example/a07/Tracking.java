c8b9436d2442f489cbeb82817001529721992e3c
Shipan <liushipan1998@gmail.com>
2023-05-16 16:37:00 +0200

    sport activity UI migrated to Tracking page

--- app/src/main/java/com/example/a07/Tracking.java
+++ app/src/main/java/com/example/a07/Tracking.java
@@ -2,9 +2,15 @@ package com.example.a07;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.Chronometer;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,11 +20,22 @@ import com.example.a07.entity.SportEntity;
 import com.example.a07.utils.SharedPreferencesUtil;
 import com.example.a07.utils.Utils;
 
+import java.util.Calendar;
 import java.util.List;
 
 public class Tracking extends AppCompatActivity implements View.OnClickListener {
 
     private SportDao sportDao;
+
+    //declare attributes for sportpage
+    protected Chronometer chronometer;
+    private Spinner sportTypeSpinner;
+    private TextView currentDate;
+    private Calendar calendar;
+    private long stopTime;
+    protected long stopTimeStored;
+
+    private Button startBtn, stopBtn, pauseBtn, addSportBtn;
 
 
     @Override
@@ -29,18 +46,78 @@ public class Tracking extends AppCompatActivity implements View.OnClickListener 
         findViewById(R.id.btn_reset_firstopen).setOnClickListener(this);
         // get sportDao
         sportDao = MyApplication.getInstance().getSportDatabase().sportDao();
-      
-      
-        //set listener for btn changing to sport tracking page 1
-        findViewById(R.id.btnSportPage).setOnClickListener(new View.OnClickListener() {
+
+
+        // Minhua
+        //connect attributes with layout elements
+        sportTypeSpinner = findViewById(R.id.spinnerSportType);
+        currentDate = findViewById(R.id.tvShowCurrentDate);
+        chronometer = findViewById(R.id.chronometerSport);
+        stopTime = 0;
+        stopTimeStored = 0;
+
+        //set current date
+        calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH) + 1;
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        currentDate.setText(day + "." + month + "." + year);
+
+        //buttons
+        startBtn = findViewById(R.id.btnSportStart);
+        stopBtn = findViewById(R.id.btnSportStop);
+        pauseBtn = findViewById(R.id.btnSportPause);
+        addSportBtn = findViewById(R.id.btnAddSport);
+
+        //set listener for the sport type spinner
+        sportTypeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent();
-                intent.setClass(Tracking.this, sportTest02.class);
-                //intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(intent);
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String content = parent.getItemAtPosition(position).toString();
+                Utils.showToast(Tracking.this, "The selected sport is: " + content);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
             }
         });
+
+
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chronometer.setBase(SystemClock.elapsedRealtime() + stopTime);
+                chronometer.start();
+            }
+        });
+
+        pauseBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chronometer.stop();
+                stopTime =  chronometer.getBase() - SystemClock.elapsedRealtime();
+            }
+        });
+
+        stopBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chronometer.stop();
+                stopTimeStored = stopTime;      //store stopTime in stopTimeStored (for later use)
+                stopTime = 0; // reset stop time
+                chronometer.setBase(SystemClock.elapsedRealtime());
+            }
+        });
+
+
+        /*
+         * addSportBtn.setOnClickListener(this);
+         * todo: add function to the add button
+         */
+
+
+      
+
     }
 
 
@@ -67,11 +144,6 @@ public class Tracking extends AppCompatActivity implements View.OnClickListener 
                 break;
         }
     }
-
-
-
-
-
 
 
 
