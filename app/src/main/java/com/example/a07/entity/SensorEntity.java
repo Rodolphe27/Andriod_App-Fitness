e500491862a6da19e8592f3577f2db3792dd7d6d
Rodolphe Souassi <87568171+Rodolphe27@users.noreply.github.com>
2023-05-22 10:04:26 +0200

    added some database file

---
+++ app/src/main/java/com/example/a07/entity/SensorEntity.java
@@ -0,0 +1,59 @@
+package com.example.a07.entity;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "SensorEntity")
+public class SensorEntity {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    private String timeAndDateStamp;
+
+    private Integer stepCount;
+
+
+
+    public SensorEntity() {
+    }
+
+    public SensorEntity(int id, String timeAndDateStamp, Integer stepCount) {
+        this.id = id;
+        this.timeAndDateStamp = timeAndDateStamp;
+        this.stepCount = stepCount;
+
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTimeAndDateStamp() {
+        return timeAndDateStamp;
+    }
+
+    public Integer getStepCount() {
+        return stepCount;
+    }
+
+
+
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTimeAndDateStamp(String timeAndDateStamp) {
+        this.timeAndDateStamp = timeAndDateStamp;
+    }
+
+    public void setX(double x) {
+        this.stepCount = stepCount;
+    }
+
+
+
+
+}
