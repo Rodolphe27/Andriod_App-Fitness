6e27eb8297817b8079f47efe2fa7cc55b1d93765
Rodolphe Souassi <87568171+Rodolphe27@users.noreply.github.com>
2023-05-28 11:59:29 +0200

    started working on digit span task

---
+++ app/src/main/java/com/example/a07/digitSpanTask.java
@@ -0,0 +1,14 @@
+package com.example.a07;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class digitSpanTask extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.digit_span_task);
+    }
+}
