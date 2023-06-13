4fa713c46d749b3b751cee9ccaaebdeaa689047e
Minh <94862685+luiminyan@users.noreply.github.com>
2023-06-13 19:46:14 +0200

    added TriggerAlgo & random exercise video Page (3.1 and 3.2)

---
+++ app/src/main/java/com/example/a07/utils/TriggerAlgo.java
@@ -0,0 +1,10 @@
+package com.example.a07.utils;
+import com.example.a07.entity.QuestionaireEntity;
+
+import java.util.HashMap;
+
+public interface TriggerAlgo {
+    //algorithm to calculate user wellbeing
+    public Boolean algo(QuestionaireEntity entity);     //algorithm 1
+
+}
